0aa5c1775a7e7fdc9df6d804b243873ad0a1c8ac
Ryan State <rwstate@gmail.com>
2019-09-17 14:01:49 -0400

    finished mvp and stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,23 +41,28 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item))
 }
 
 /* STRETCH PROBLEM */
@@ -66,4 +71,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  uniques= []
+  array.filter((el) => {
+    if (uniques.includes(el) === false) {
+      uniques.push(el)
+    }
+  })
+  cb(uniques)
 }
